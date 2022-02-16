bd559b0f9ee6808b426c2d7210d93ba454fe3073
Zephyrrus <zachary.ehle@gmail.com>
2022-02-16 09:31:11 -0500

    Edited name
    Changed from gridThings-Zephyrrus
    to gridThingZ

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gridThings-Zephyrrus",
-    version="0.0.1",
+    name="gridThingz",
+    version="1.0.0",
     author="Zephyrrus",
     author_email="zachary.ehle@gmail.com",
     description="A small package made to modify the numpy 2d data structure systed for ease of use.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Zachary99/gridThings/",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/Zachary99/gridThingZ/issues",
+        "Wiki": "https://github.com/Zachary99/gridThings/wiki",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
