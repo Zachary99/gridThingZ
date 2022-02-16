8e790a05aab9e6fa4b3aefd79a43fe8e2ec342ee
Zephyrrus <zachary.ehle@gmail.com>
2022-02-16 08:05:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gridThings-Zephyrrus",
-    version="0.0.1",
+    version="1.0.1",
     author="Zephyrrus",
     author_email="zachary.ehle@gmail.com",
     description="A small package made to modify the numpy 2d data structure systed for ease of use.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Zachary99/gridThings",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/Zachary99/gridThings/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
